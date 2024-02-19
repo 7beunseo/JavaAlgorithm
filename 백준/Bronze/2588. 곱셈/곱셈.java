b9c4befedfca92d1652aea9879609ac5139e60fd
EunseoKim <128278212+7beunseo@users.noreply.github.com>
2024-02-20 00:48:26 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,25 +1,16 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int A = sc.nextInt();
-        int B = sc.nextInt();
+        String B =sc.next();
         
-        int first = B / 100;
-        int second = (B % 100) / 10;
-        int third = B % 10;
-        
-        int top1 = A * third;
-        int top2 = A * second;
-        int top3 = A * first;
-        
-        int top4 = top1 + top2 * 10 + top3 * 100;
-        
-        System.out.println(top1);
-        System.out.println(top2);
-        System.out.println(top3);
-        System.out.println(top4);
+        System.out.println(A*(B.charAt(2)-'0'));
+        System.out.println(A*(B.charAt(1)-'0'));
+        System.out.println(A*(B.charAt(0)-'0'));
+        System.out.println(A*Integer.parseInt(B));
         
     }
 }
